f17020d0105bc46327bd7eee70d4c9a02c4c4ab4
Dasun Chamara <dasunchamara0811@gmail.com>
2025-04-15 23:09:50 +0530

    home,search

--- fly/src/Component/Search/Search.jsx
+++ fly/src/Component/Search/Search.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { HiOutlineLocationMarker } from "react-icons/hi";
+
 
 function Search() {
   return (
@@ -6,17 +8,46 @@ function Search() {
       <div className="sectionContainer grid">
         
         <div className="btns flex">
-
           <div className="singleBtn">
             <span>Economy</span>
           </div>
 
           <div className="singleBtn">
-            <span>Business</span>
+            <span>Business Class</span>
           </div>
 
 
         </div>
+
+        <div className="searchInputs flex">
+              <div className="singleInput flex">
+                    <div className="iconDiv">
+                        <HiOutlineLocationMarker className='icon'/>
+                    </div>
+                    <div className="texts">
+                      <h4>
+                        Location
+                      </h4>
+                      <input type='text' placeholder='Where do you want to go?' />
+                    </div>
+              </div>
+
+            
+              <div className="singleInput flex">
+                    <div className="iconDiv">
+                        <HiOutlineLocationMarker className='icon'/>
+                    </div>
+                    <div className="texts">
+                      <h4>
+                        Location
+                      </h4>
+                      <input type='text' placeholder='Where do you want to go?' />
+                    </div>
+              </div>
+              
+
+        </div>
+
       </div>
       
     </div>
